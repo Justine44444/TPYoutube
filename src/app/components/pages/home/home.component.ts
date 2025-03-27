292f80d817b469d55afc56cf6fcb1f1c5a6e9367
Justine44444 <137166047+Justine44444@users.noreply.github.com>
2025-03-27 14:05:44 +0100

    Ajout de la side bar

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
+import { HeaderComponent } from '../../header/header.component';
+import { SidebarComponent } from '../../sidebar/sidebar.component';
+
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SidebarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
